1bc5fa7e698556d011bddac49a7326240c3a7d86
João Paulo <joaopaulloojpos15@gmail.com>
2021-10-13 03:11:19 -0300

    Altera configurações do servidor e package.json

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+//Importa as dependências que acabamos de instalar
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/secosandpile'));
+// Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
+app.use(express.static(__dirname + "/dist/secosandpile"));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/secosandpile/'}),
-);
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/secosandpile/index.html"));
+});
 
-// Start the app by listening on the default Heroku port
+// Inicia a aplicação pela porta configurada
 app.listen(process.env.PORT || 8080);
